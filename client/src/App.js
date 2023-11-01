e4cc1d21f4136c06f1381012bafe926adce6e1de
victor odambatafwa <omondivictoras@gmail.com>
2023-11-01 15:31:04 +0300

    design of login,signup and profile components

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from "react";
 import './App.css';
+//import Navbar from "./Components/Navbar";
+import Login from "./Components/Login";
+import Profile from "./Components/Profile";
+
+// import { Outlet } from 'react-router-dom';
+// import Router from "./Components/Router";
+
 
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <div>
+
+           {/* <Navbar />   
+           <Router />    */}
+           <Profile />
+          {/* <Outlet />   */}
+         
+         
+         
+        </div> 
+  
   );
 }
 
